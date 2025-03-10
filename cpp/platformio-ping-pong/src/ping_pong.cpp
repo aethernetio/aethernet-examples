ae585ee2a99b6bfbc7f52d1d7513c7fd09f4f498
BartolomeyKant <notevill@gmail.com>
2025-03-10 18:44:06 +0500

    fix create adapter and facility

--- cpp/platformio-ping-pong/src/ping_pong.cpp
+++ cpp/platformio-ping-pong/src/ping_pong.cpp
@@ -152,13 +152,13 @@ int AetherPingPongExample() {
 #if !AE_SUPPORT_REGISTRATION
           []() {
             auto fs =
-                ae::MakePtr<ae::FileSystemHeaderFacility>(std::string(""));
+                ae::make_unique<ae::FileSystemHeaderFacility>(std::string(""));
             return fs;
           }
 #endif  // AE_SUPPORT_REGISTRATION
       }
 #if defined AE_DISTILLATION
-          .Adapter([](ae::Ptr<ae::Domain> const& domain,
+          .Adapter([](ae::Domain* domain,
                       ae::Aether::ptr const& aether) -> ae::Adapter::ptr {
 #  if defined ESP32_WIFI_ADAPTER_ENABLED
             auto adapter = domain->CreateObj<ae::Esp32WifiAdapter>(
