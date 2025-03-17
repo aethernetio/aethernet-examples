6c536d8df338d0ffadecd6f88df578012f910133
BartolomeyKant <notevill@gmail.com>
2025-03-17 12:36:07 +0500

    add ping before start

--- cpp/desktop-ping-pong/build_and_run.bat
+++ cpp/desktop-ping-pong/build_and_run.bat
@@ -1,3 +1,19 @@
+@echo off
+REM Copyright 2025 Aethernet Inc.
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+@echo on
+
 git submodule update --init --remote aether-client-cpp
 cd aether-client-cpp
 call git_init.bat
@@ -5,6 +21,11 @@ cd ..
 mkdir build-example
 cd build-example
 cmake ..
-cmake --build .
-cd Debug
+cmake --build . --config Release
+cd Release
+
+echo "Get a reference ping to the Aethernet infrastructure"
+ping cloud.aethernet.io
+
+echo "Run example"
 ping-pong-example.exe
--- cpp/desktop-ping-pong/build_and_run.sh
+++ cpp/desktop-ping-pong/build_and_run.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+# Copyright 2025 Aethernet Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 git submodule update --init --remote aether-client-cpp
 cd aether-client-cpp
 ./git_init.sh
@@ -6,4 +21,9 @@ mkdir build-example
 cd build-example
 cmake ..
 cmake --build .
+
+echo "Get a reference ping to the Aethernet infrastructure"
+ping cloud.aethernet.io -c 5
+
+echo "Run example"
 ./ping-pong-example
