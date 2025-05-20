1ecd65dc3c9b141a8b1ed9ed07ce795d3748b716
BartolomeyKant <notevill@gmail.com>
2025-05-20 15:28:01 +0500

    remove send telemetry

--- cpp/platformio-ping-pong/src/ping_pong.cpp
+++ cpp/platformio-ping-pong/src/ping_pong.cpp
@@ -203,8 +203,8 @@ int AetherPingPongExample() {
     auto [client_alice, client_bob] = action.get_clients();
     alice = ae::make_unique<Alice>(*aether_app, std::move(client_alice),
                                    time_synchronizer, client_bob->uid());
-    bob = ae::make_unique<Bob>(*aether_app, client_bob, time_synchronizer);
-    client_bob->client_connection()->SendTelemetry();
+    bob = ae::make_unique<Bob>(*aether_app, std::move(client_bob),
+                               time_synchronizer);
     // Save current aether state
     aether_app->domain().SaveRoot(aether_app->aether());
   });
